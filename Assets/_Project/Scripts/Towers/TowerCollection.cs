28731765d135640476777397477a064371c03ec2
Igor Lamounier <igorindev@gmail.com>
2024-01-16 17:38:53 -0300

    Remove unused lines

--- Assets/_Project/Scripts/Towers/TowerCollection.cs
+++ Assets/_Project/Scripts/Towers/TowerCollection.cs
@@ -1,10 +1,7 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 namespace Collection
 {
-
     [CreateAssetMenu]
     public class TowerCollection : Collection<TowerData>
     {
